fca50f404d6c2e24b39776ea9c0acab69ecdcc42
millooo94 <camillomusmeciunict@gmail.com>
2023-03-20 17:20:59 +0100

    refactor all

---
+++ Product.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Product
+{
+    public Product(string name)
+    {
+        Name = name;
+        var randomNumber = new Random();
+        Code = randomNumber.Next(10, 500);
+    }
+    public int Code { get; private set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+    public long Iva { get; set; }
+
+    public string GetUntaxedPrice()
+    {
+        var untaxedPrice = Price + "$";
+        return untaxedPrice;
+    }
+
+    public string GetTaxedPrice()
+    {
+        var taxedPrice = Math.Round(Price - (Price * 22 / 100)) + "$";
+        return taxedPrice;
+    }
+
+    public string GetExtendedName()
+    {
+        return Code + " " + Name;
+    }
+
+    public string GetPaddedCode()
+    {
+        var stringCode = Convert.ToString(Code);
+        var zero = "0";
+        var iterations = 8 - stringCode.Length;
+        
+        for (int i = 0; i < iterations; i++)
+        {
+            stringCode = zero + stringCode;
+        }
+
+        return stringCode; 
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var productNames = new string[] { "Pen", "Pencil", "Eraser" };
+var products = new Product[3];
+
+for (int i = 0; i < products.Length; i++)
+{
+    var productIndex = productNames[i];
+    var product = new Product(productIndex);
+    product.Description = "This is " + productNames[i] + " number " + (i + 1);
+    product.Price = 2.99 + i;
+    if (i == 0)
+    {
+        product.Iva = 26543987632;
+    } else
+    {
+        product.Iva = i * 123456789;
+    }
+    var untaxedPrice = product.GetUntaxedPrice();
+    var taxedPrice = product.GetTaxedPrice();
+    var extendedName = product.GetExtendedName();
+    var paddedCode = product.GetPaddedCode();
+    products[i] = product;
+    Console.WriteLine(product.Name);
+    Console.WriteLine(product.Description);
+    Console.WriteLine(product.Price);
+    Console.WriteLine(product.Iva);
+    Console.WriteLine(untaxedPrice);
+    Console.WriteLine(taxedPrice);
+    Console.WriteLine(extendedName);
+    Console.WriteLine(paddedCode);
+}
+
+
+
+
